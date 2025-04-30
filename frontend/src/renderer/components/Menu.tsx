95e59ea5860f8ecb08d632ccbe8628a46a97dea5
xmarimarquesh <marimarquesh39@gmail.com>
2025-04-30 17:08:08 -0300

    ta

--- frontend/src/renderer/components/Menu.tsx
+++ frontend/src/renderer/components/Menu.tsx
@@ -1,24 +1,26 @@
 import '../App.css';
 import linha from '../../../assets/linha.jpg'
 import logo from '../../../assets/logo.png'
+import notification from '../../../assets/notification.png'
 
 function Menu() {
     return (
       <>
-        <div className='Menu'>
+        <div className='AbaSuperior'>
           <img alt="icon" src={linha} className='Imagem'/>
           <div className='Logo'>
             <img src={logo} alt="BatBrasil"/>
           </div>
         </div>
-        <div>
+        <div className='Menu'>
           <div>
-            <a href="">Dashboards</a>
-            <a href="">Chamados</a>
-            <a href="">Máquinas</a>
+            <a href="" className='text_menu'>Dashboards</a>
+            <a href="" className='text_menu'>Chamados</a>
+            <a href="" className='text_menu'>Máquinas</a>
           </div>
-          <div>
-            <img src="../../../assets/notification.png" alt="" />
+          <div className='Not'>
+            <img src={notification} alt="notifications" />
+            <a href="" className='text_menu'>Sair</a>
           </div>
         </div>
       </>

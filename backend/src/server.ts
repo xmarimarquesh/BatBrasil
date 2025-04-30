1dea6794373b5a609965bfdfffe06ec42e009685
Helengendary <helenapicinin13@gmail.com>
2025-04-30 17:06:06 -0300

    todas as tabelas criadas no prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,7 @@
+// npm i prisma @prisma/client -D
+// npx prisma init --datasource-provider mysql
+// npx prisma migrate dev
+// npm run dev
 import express from 'express';
 import initRoutes from './routes/routes.js'
 
@@ -6,8 +10,8 @@ initRoutes(app)
 
 const port = 8080;
 
-app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));
-
 app.get('/getTeste', (req, res) => {
     res.send('GET: Requisição recebida com sucesso!');
 });
+
+app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));

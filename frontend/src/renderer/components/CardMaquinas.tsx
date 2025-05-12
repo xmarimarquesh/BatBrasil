091d6cbe2b024205b48e61f8fdf177e297892585
juupurcino <jdiaspurcino@gmail.com>
2025-05-12 17:09:35 -0300

    card quase pronto

--- frontend/src/renderer/components/CardMaquinas.tsx
+++ frontend/src/renderer/components/CardMaquinas.tsx
@@ -1,20 +1,37 @@
 import '../css/cardMaquinas.css';
 import { Link, Route, Routes } from 'react-router-dom';
 
+var dataChamado = '13/07/2024 13:45';
+var funcionario = 'Juliana Dias Purcino';
+var tecnico = 'Juliana Dias Purcino';
+var descricao = 'Peça da máquina com deifeito na bomba';
+
 function CardMaquinas() {
   return (
     <>
-      <div>
-        <div className='tempo'>
-          <div className='bolaTempo'></div>
-          <div className='linhaTempo'></div>
+      <div className="mainCard">
+        <div className="tempo">
+          <div className="bolaTempo"></div>
+          <div className="linhaTempo"></div>
         </div>
-        <div>
-          <div></div>
-          <div className='card'>
-            <h1>Chamado aberto por:</h1>
-            <h1>De:</h1>
-            <h1>Chamado aberto por:</h1>
+        <div className="pagCard">
+          <div className="data">{dataChamado}</div>
+          <div className="card">
+            <div className="font">Chamado aberto por:{funcionario}</div>
+            <div className='desc'>
+              <div className="font">Descrição: </div>
+              <div className="inputDesc">{descricao}</div>
+            </div>
+            <div className='desc'>
+              <div className="font">Feedback: </div>
+              <div className="inputDesc">{descricao}</div>
+            </div>
+            <div>
+              
+
+            </div>
+
+            <div className="font">Técnico: {tecnico}</div>
           </div>
         </div>
       </div>
--- frontend/src/renderer/pages/Maquinas.tsx
+++ frontend/src/renderer/pages/Maquinas.tsx
@@ -1,14 +1,16 @@
 import '../css/App.css';
 import Menu from '../components/Menu';
 import CardMaquinas from '../components/CardMaquinas';
+import '../css/maquinas.css';
 
 export default function Maquinas() {
   return (
     <>
-      <Menu/>
-      <h1>Maquinas</h1>
-      <CardMaquinas/>
+      <Menu />
+      <div className='main'>
+        <h1>Maquinas</h1>
+        <CardMaquinas />
+      </div>
     </>
   );
 }
-

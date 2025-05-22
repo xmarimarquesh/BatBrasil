02ef54b4061a44bf91222c6b0f7365d80f4cc1cb
Helengendary <helenapicinin13@gmail.com>
2025-05-22 15:56:39 -0300

    Início da estrutura do back e login com conexão do front feita

--- frontend/src/renderer/components/Menu.tsx
+++ frontend/src/renderer/components/Menu.tsx
@@ -4,8 +4,35 @@ import logo from '../../../assets/logo.png'
 import notification from '../../../assets/notification.png'
 import { Link, Route, Routes } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+
+interface User {
+  id: number,
+  nome: string,
+  funcao: number
+}
 
 function Menu() {
+
+  const [user, setUsuario] = useState<User | null>(null)
+
+  useEffect(() => {
+    fetch("http://localhost:5000/session", {
+      method: "GET",
+      credentials: "include", // ESSENCIAL para enviar cookies da sessão
+    })
+      .then(res => res.json())
+      .then(user => {
+        if (!user.error) {
+          setUsuario(user);
+        }
+      });
+  }, []);
+
+  if (!user) {
+    return <p>Carregando informações da sessão...</p>;
+  }
+
     return (
       <>
         <div className='AbaSuperior'>
@@ -20,7 +47,9 @@ function Menu() {
           <NavLink to="/chamados" className={({ isActive }) => isActive ? 'text_menu active' : 'text_menu'}>Chamados</NavLink>
           <NavLink to="/maquinas" className={({ isActive }) => isActive ? 'text_menu active' : 'text_menu'}>Máquinas</NavLink>
           </div>
-          <div className='Not'>
+
+          <div className='Not'>            
+            {user && <h3>{user.nome}</h3>}
             <img src={notification} alt="notifications" />
             <Link to="/" className='text_menu'>Sair</Link>
           </div>

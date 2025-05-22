646f5dbb9bfa434834aa2b2937123f05a80b7a3a
juupurcino <jdiaspurcino@gmail.com>
2025-05-22 14:46:57 -0300

    máquinas pronto

--- frontend/src/renderer/pages/Maquinas.tsx
+++ frontend/src/renderer/pages/Maquinas.tsx
@@ -3,6 +3,7 @@ import Menu from '../components/Menu';
 import CardMaquina from '../components/CardMaquina';
 import CardHistMaquina from '../components/CardHistMaquina';
 import '../css/maquina.css';
+import Modal from '../components/Modal';
 
 type Historico = {
   texto: string;
@@ -29,81 +30,108 @@ type Chamado = {
 };
 
 export default function Maquinas() {
-  const [maquinaSelecionadaId, setMaquinaSelecionadaId] = useState<number | null>(null);
+  const [maquinaSelecionadaId, setMaquinaSelecionadaId] = useState<
+    number | null
+  >(null);
+  const [isModalOpen, setModalOpen] = useState(false);
 
   const maquinas: Maquina[] = [
     {
       id: 1,
-      descricao: "Enrolador de cigarro",
-      dataCompra: "12/09/2023",
-      data: "12/09/2023",
-      idSetor: "DU"
+      descricao: 'Enrolador de cigarro',
+      dataCompra: '12/09/2023',
+      data: '12/09/2023',
+      idSetor: 'DU',
     },
     {
       id: 2,
-      descricao: "Seladora de pacotes",
-      dataCompra: "15/08/2023",
-      data: "15/08/2023",
-      idSetor: "IN"
-    }
+      descricao: 'Seladora de pacotes',
+      dataCompra: '15/08/2023',
+      data: '15/08/2023',
+      idSetor: 'IN',
+    },
   ];
 
   const chamados: Chamado[] = [
     {
       id: 101,
       idMaquina: 1,
-      funcionario: "Juliana Dias",
-      tecnico: "João Silva",
-      descricao: "Peça da máquina com defeito",
-      feedback: "Peça trocada com sucesso",
-      dataChamado: "13/07/2024 13:45",
+      funcionario: 'Juliana Dias',
+      tecnico: 'João Silva',
+      descricao: 'Peça da máquina com defeito',
+      feedback: 'Peça trocada com sucesso',
+      dataChamado: '13/07/2024 13:45',
       historico: [
-        { texto: "Chamado aberto", data: "13/07/2024 13:45" },
-        { texto: "Peça verificada", data: "13/07/2024 13:50" },
-        { texto: "Peça trocada", data: "13/07/2024 14:10" },
-        { texto: "Chamado finalizado", data: "13/07/2024 15:10" },
-      ]
+        { texto: 'Chamado aberto', data: '13/07/2024 13:45' },
+        { texto: 'Em andamento', data: '13/07/2024 13:50' },
+        { texto: 'Chamado Finalizado', data: '13/07/2024 14:10' },
+      ],
     },
     {
       id: 102,
       idMaquina: 1,
-      funcionario: "Carlos Silva",
-      tecnico: "Ana Souza",
-      descricao: "Barulho incomum no motor",
-      feedback: "Lubrificação aplicada",
-      dataChamado: "20/07/2024 09:10",
+      funcionario: 'Carlos Silva',
+      tecnico: 'Ana Souza',
+      descricao: 'Barulho incomum no motor',
+      feedback: 'Lubrificação aplicada',
+      dataChamado: '20/07/2024 09:10',
       historico: [
-        { texto: "Chamado aberto", data: "20/07/2024 09:10" },
-        { texto: "Avaliação técnica", data: "20/07/2024 09:25" },
-        { texto: "Lubrificação aplicada", data: "20/07/2024 09:40" },
-        { texto: "Chamado finalizado", data: "20/07/2024 10:00" },
-      ]
+        { texto: 'Chamado aberto', data: '20/07/2024 09:10' },
+        { texto: 'Avaliação técnica', data: '20/07/2024 09:25' },
+        { texto: 'Lubrificação aplicada', data: '20/07/2024 09:40' },
+        { texto: 'Chamado finalizado', data: '20/07/2024 10:00' },
+      ],
     },
     {
       id: 103,
       idMaquina: 2,
-      funcionario: "Maria Oliveira",
-      tecnico: "Pedro Lima",
-      descricao: "Falha no aquecimento",
-      feedback: "Sensor substituído",
-      dataChamado: "22/07/2024 10:20",
+      funcionario: 'Maria Oliveira',
+      tecnico: 'Pedro Lima',
+      descricao: 'Falha no aquecimento',
+      feedback: 'Sensor substituído',
+      dataChamado: '22/07/2024 10:20',
       historico: [
-        { texto: "Chamado aberto", data: "22/07/2024 10:20" },
-        { texto: "Sensor verificado", data: "22/07/2024 10:30" },
-        { texto: "Sensor trocado", data: "22/07/2024 10:50" },
-        { texto: "Chamado finalizado", data: "22/07/2024 11:00" },
-      ]
-    }
+        { texto: 'Chamado aberto', data: '22/07/2024 10:20' },
+        { texto: 'Sensor verificado', data: '22/07/2024 10:30' },
+        { texto: 'Sensor trocado', data: '22/07/2024 10:50' },
+        { texto: 'Chamado finalizado', data: '22/07/2024 11:00' },
+      ],
+    },
   ];
 
   const handleSelecionarMaquina = (id: number) => {
-    setMaquinaSelecionadaId(prev => (prev === id ? null : id));
+    setMaquinaSelecionadaId((prev) => (prev === id ? null : id));
   };
 
   return (
     <>
       <Menu />
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        title="Cadastrar máquina"
+      >
+        <div className="modal">
+          <label>Nome máquina</label>
+          <input type="text" />
+          <label>Data compra</label>
+          <input type="date" />
+
+          <label>Setor</label>
+          <select name="maquina" id="maquina">
+            <option value="maquina1">Máquina 1</option>
+            <option value="maquina2">Máquina 2</option>
+            <option value="maquina3">Máquina 3</option>
+            <option value="maquina4">Máquina 4</option>
+          </select>
+          <button className="button">Cadastrar</button>
+        </div>
+      </Modal>
       <div className="main">
+        <div className='abrir-chamado'>
+          <button onClick={() => setModalOpen(true)}>Cadastrar máquina</button>
+        </div>
         <div className="maquinas">
           {maquinas.map((maquina) => (
             <div key={maquina.id}>
@@ -111,10 +139,12 @@ export default function Maquinas() {
                 <CardMaquina {...maquina} />
               </div>
 
-              <div className={`historico ${maquinaSelecionadaId === maquina.id ? 'aberto' : ''}`}>
+              <div
+                className={`historico ${maquinaSelecionadaId === maquina.id ? 'aberto' : ''}`}
+              >
                 {maquinaSelecionadaId === maquina.id &&
                   chamados
-                    .filter(chamado => chamado.idMaquina === maquina.id)
+                    .filter((chamado) => chamado.idMaquina === maquina.id)
                     .map((chamado) => (
                       <CardHistMaquina
                         key={chamado.id}
@@ -126,8 +156,7 @@ export default function Maquinas() {
                         feedback={chamado.feedback}
                         historico={chamado.historico}
                       />
-                    ))
-                }
+                    ))}
               </div>
             </div>
           ))}

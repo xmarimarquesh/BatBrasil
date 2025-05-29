56b0435d6f8a11a0e66ba05a112119bee5e316c6
xmarimarquesh <marimarquesh39@gmail.com>
2025-05-29 17:16:09 -0300

    ta

--- frontend/src/renderer/components/UserContext.tsx
+++ frontend/src/renderer/components/UserContext.tsx
@@ -9,26 +9,34 @@ interface User {
 
 interface UserContextType {
   user: User | null;
-  setUser: (user: User) => void;
+  setUser: (user: User | null) => void;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      fetch("http://localhost:5000/session", {
-        method: "GET",
-        credentials: "include",
+    fetch("http://localhost:5000/session", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then(res => {
+        if (!res.ok) throw new Error("Sessão não encontrada");
+        return res.json();
       })
-        .then(res => res.json())
-        .then(data => {
-          setUser(data);
-        });
-    
-  }, []);
-
+      .then(data => {
+        setUser(data);
+      })
+      .catch(() => {
+        setUser(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [user]);
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}

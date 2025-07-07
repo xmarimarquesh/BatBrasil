b78ffd74828fc73492f67e25cfa3809a452d5690
juupurcino <jdiaspurcino@gmail.com>
2025-07-06 22:38:33 -0300

    pronto\\\\\\'

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,5 @@
-host = 'localhost'
+host = 'bpj0hakwipseyrx3j6a8-mysql.services.clever-cloud.com'
 database = 'bpj0hakwipseyrx3j6a8'
-user = 'root'
-password = 'root'
+user = 'uwvkzbpqad0ua2kq'
+password = 'dJjJaiSsuououE96wjrh'
 port = 3306
